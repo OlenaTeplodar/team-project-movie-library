b99ffd3f8cd60788b7fa4131b4af3db89c63360e
IraGubish2022 <iragubish@gmail.com>
2023-01-04 20:51:09 +0200

    обновленный спиннер

---
+++ src/js/spinner.js
@@ -0,0 +1,26 @@
+
+import {Spinner} from 'spin.js';
+
+const opts = {
+  lines: 13, 
+  length: 38, 
+  width: 17, 
+  radius: 45, 
+  scale: 1, 
+  corners: 1, 
+  speed: 1, 
+  rotate: 0, 
+  animation: 'spinner-line-fade-more', 
+  direction: 1, 
+  color: '#FF001B', 
+  fadeColor: 'transparent', 
+  top: '50%', 
+  left: '50%', 
+  shadow: '0 0 1px transparent',
+  zIndex: 2000000000, 
+  className: 'spinner', 
+  position: 'absolute', 
+};
+
+const target = document.getElementById('foo');
+export const spinner = new Spinner(opts).spin(target);
