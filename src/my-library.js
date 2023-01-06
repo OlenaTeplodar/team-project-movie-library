d177e91b9aee56cd1a3ae770e9ddc2a72d11c199
Tatiana-Hela <malya9472@gmail.com>
2023-01-07 01:20:58 +0300

    queue-library done

--- src/my-library.js
+++ src/my-library.js
@@ -1,5 +1,8 @@
 import './js/watched-library';
+import './js/queue-library';
 import './js/about-team-modal';
 import { btnUp } from './js/button-up';
-
+import { onWatchedLibrary } from './js/watched-library';
 btnUp.addEventListener();
+
+onWatchedLibrary();
