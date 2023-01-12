09d50d05b86ff8f6f8f9d11594aaeb98c59544ad
olenaTeplodar <lena.golubenkova@gmail.com>
2023-01-12 13:32:17 +0200

    off register

--- src/js/login-modal.js
+++ src/js/login-modal.js
@@ -1,45 +1,45 @@
-import { refs } from './refs-firebase';
+// import { refs } from './refs-firebase';
 
-refs.enterBtn.addEventListener('click', onOpenLoginModal);
+// refs.enterBtn.addEventListener('click', onOpenLoginModal);
 
-refs.modalWindow.addEventListener('click', closeEnterModal);
+// refs.modalWindow.addEventListener('click', closeEnterModal);
 
-export function onOpenLoginModal() {
-  refs.modalWindow.classList.add('visible');
-  refs.bodyAsFone.classList.add('no-scroll');
-  window.addEventListener('keydown', onEscClick);
-}
+// export function onOpenLoginModal() {
+//   refs.modalWindow.classList.add('visible');
+//   refs.bodyAsFone.classList.add('no-scroll');
+//   window.addEventListener('keydown', onEscClick);
+// }
 
-function onEscClick(evt) {
-  if (evt.code === 'Escape') {
-    closeEnterModal();
-    window.removeEventListener('keydown', onEscClick);
-  }
-}
+// function onEscClick(evt) {
+//   if (evt.code === 'Escape') {
+//     closeEnterModal();
+//     window.removeEventListener('keydown', onEscClick);
+//   }
+// }
 
-function closeEnterModal(evt) {
-  if (evt.target === refs.registrForm) {
-    refs.modalWindow.classList.remove('visible');
-    refs.registrForm.classList.remove('hidden');
-    refs.loginForm.classList.remove('visible');
-    onBodyScroll();
-  }
+// function closeEnterModal(evt) {
+//   if (evt.target === refs.registrForm) {
+//     refs.modalWindow.classList.remove('visible');
+//     refs.registrForm.classList.remove('hidden');
+//     refs.loginForm.classList.remove('visible');
+//     onBodyScroll();
+//   }
 
-  if (evt.target === refs.loginForm) {
-    refs.loginForm.classList.remove('active');
-    onBodyScroll();
-  }
-}
+//   if (evt.target === refs.loginForm) {
+//     refs.loginForm.classList.remove('active');
+//     onBodyScroll();
+//   }
+// }
 
-function onBodyScroll() {
-  refs.bodyAsFone.classList.remove('no-scroll');
-}
+// function onBodyScroll() {
+//   refs.bodyAsFone.classList.remove('no-scroll');
+// }
 
-const refs = {
-  registrBtn: document.querySelector('.registr-btn'),
-  modalWindow: document.querySelector('.modal-window'),
-  exitBtn: document.querySelector('.exitBtnForm'),
-  bodyAsFone: document.querySelector('body'),
-  loginBtn: document.querySelector('.login-btn'),
-  enterBtn: document.querySelector('.enter-btn'),
-};
+// const refs = {
+//   registrBtn: document.querySelector('.registr-btn'),
+//   modalWindow: document.querySelector('.modal-window'),
+//   exitBtn: document.querySelector('.exitBtnForm'),
+//   bodyAsFone: document.querySelector('body'),
+//   loginBtn: document.querySelector('.login-btn'),
+//   enterBtn: document.querySelector('.enter-btn'),
+// };
